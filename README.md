4b76a406d6789756996976744669399447e3de9e
diophantus7 <bademeister7@web.de>
2020-04-07 21:30:56 +0200

    added README

---
+++ README.md
@@ -0,0 +1,4 @@
+# romlib library
+
+This is a simple library to scrape content from the romwod page
+
--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
-import pathlib
 import setuptools
- 
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
- 
-# The text of the README file
-#README = (HERE / "README.rst").read_text()
+
+with open("README.md", "r") as f:
+    long_description = f.read()
  
 # This call to setup() does all the work
 setuptools.setup(
     name="romlib",
-    version="0.1",
-    description="A library to scrape the romwod page",
+    version="1.0",
     author="diophantus7",
+    author_email="stefgmz@guffin.de",
+    description="A library to scrape the romwod page",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/diophantus7/romlib/",
     classifiers=[
-        "Programming Language :: Python"
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: Apache License 2.0",
+        "Topic :: Software Development :: Libraries",
+        "Intended Audience :: Developers",
     ],
     packages=setuptools.find_packages(),
     install_requires=['beautifulsoup4', 'requests'],
